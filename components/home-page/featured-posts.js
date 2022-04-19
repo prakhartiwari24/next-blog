7de3115ae20f89076caee219c6474b602b493f98
prakhar tiwari <prakhartiwari20@gmail.com>
2022-04-19 21:20:49 +0530

    continue from 185

--- components/home-page/featured-posts.js
+++ components/home-page/featured-posts.js
@@ -1,5 +1,13 @@
-function FeaturedPosts() {
-	return <p>FeaturedPosts</p>;
+import classes from "./featured-posts.module.css";
+import PostGrid from "../posts/post-grid";
+
+function FeaturedPosts(props) {
+	return (
+		<section className={classes.latest}>
+			<h2>Featured posts</h2>
+			<PostGrid posts={props.posts} />
+		</section>
+	);
 }
 
 export default FeaturedPosts;
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,42 @@ import { Fragment } from "react";
 import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
 
+const DUMMY_POSTS = [
+	{
+		slug: "getting-started-with-nextjs",
+		title: "Getting started with nextjs",
+		image: "getting-started-nextjs.png",
+		excerpt: "Nextjs is the react framework for prodcution",
+		date: "2022-02-10",
+	},
+	{
+		slug: "getting-started-with-nextjs2",
+		title: "Getting started with nextjs2",
+		image: "getting-started-nextjs.png",
+		excerpt: "Nextjs is the react framework for prodcution",
+		date: "2022-02-10",
+	},
+	{
+		slug: "getting-started-with-nextjs3",
+		title: "Getting started with nextjs3",
+		image: "getting-started-nextjs.png",
+		excerpt: "Nextjs is the react framework for prodcution",
+		date: "2022-02-10",
+	},
+	{
+		slug: "getting-started-with-nextjs4",
+		title: "Getting started with nextjs4",
+		image: "getting-started-nextjs.png",
+		excerpt: "Nextjs is the react framework for prodcution",
+		date: "2022-02-10",
+	},
+];
+
 function HomePage() {
 	return (
 		<Fragment>
 			<Hero />
+			<FeaturedPosts posts={DUMMY_POSTS} />
 		</Fragment>
 	);
 }
